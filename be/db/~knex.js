d95781080cea1d5a8516c73ec861d46eeaf46020
Nikita Shevchenko <nicksonsheva0609@gmail.com>
2020-08-25 00:50:16 +0300

    fast commit

--- be/db/~knex.js
+++
@@ -1,5 +0,0 @@
-import knex from 'knex';
-import process from 'process';
-import config from '../knexfile';
-
-export default knex(config[process.env.NODE_ENV || 'development']); // TODO: || -> ??
--- be/db/~objection.js
+++
@@ -1,6 +0,0 @@
-import { Model, } from 'objection';
-import knex from './~knex';
-
-Model.knex(knex);
-
-export default knex;
