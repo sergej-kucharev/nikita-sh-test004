241f96a0a53d4f786fb503ff97a2857c37856276
Sergej Kucharev <sergej.kucharev@gmail.com>
2020-08-25 03:03:00 +0300

    fast commit

--- be/db/index.js
+++ be/db/index.js
@@ -2,8 +2,8 @@ import knex from 'knex';
 import process from 'process';
 import { Model, } from 'objection';
 
-import configs from '../knexfile';
-import models from './models';
+import configs from '../knexfile.js';
+import models from './models/index.js';
 
 const config = configs[process.env.NODE_ENV || 'development']; // TODO: || -> ??
 const knexInstance = knex(config);
--- be/knexfile.js
+++ be/knexfile.js
@@ -1,7 +1,7 @@
 // Update with your config settings.
 
 import dotenv from 'dotenv';
-import sqlite3 from './db/config/sqlite3';
+import sqlite3 from './db/config/sqlite3.js';
 
 dotenv.config();
 export default {
