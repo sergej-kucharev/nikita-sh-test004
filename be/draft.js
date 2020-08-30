67fa04ed09cec2e307b1141e7832ae8fe304eb20
Sergej Kucharev <sergej.kucharev@gmail.com>
2020-08-31 00:45:38 +0300

    vscode gitignore

--- be/draft.js
+++ be/draft.js
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 require('@babel/register');
 
 const { knex, models, } = require('./db/knex');
--- be/index.js
+++ be/index.js
@@ -1,11 +1,4 @@
 #!/usr/bin/env node
 
-const register = require('@babel/register');
-
-register({
-	presets: [
-		'@babel/preset-env'
-	],
-});
-
-module.exports = require('./src/app.js');
+require('@babel/register');
+require('./src/app.js');
