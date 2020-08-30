6279ca8ac7726ffb8c34bd0e15dad19a0ad83863
Sergej Kucharev <sergej.kucharev@gmail.com>
2020-08-31 00:36:38 +0300

    fast commit

--- be/db/knex/config/development.sqlite3.js
+++ be/db/knex/config/development.sqlite3.js
@@ -1,37 +1,33 @@
 import Debug from 'debug';
 import sqlite3 from 'sqlite3';
 
+export const client = 'sqlite3';
+export const connection = {
+  filename: './db/knex/data/development.sqlite3',
+};
+export const migrations = {
+  directory: './db/knex/migrations',
+  tableName: 'migrations',
+};
+export const seeds = {
+  directory: './db/knex/seeds',
+  tableName: 'seeds',
+};
+
+export const acquireConnectionTimeout = 1000 * 8;
+export const asyncStackTraces = true;
+export const fetchAsString = [ 'clob', ];
+export const useNullAsDefault = true;
+
 const debug = Debug('db:knex');
-
-const config = {
-  client: 'sqlite3',
-  connection: {
-    filename: './db/knex/data/development.sqlite3',
-  },
-  migrations: {
-    directory: './db/knex/migrations',
-    tableName: 'migrations',
-  },
-  seeds: {
-    directory: './db/knex/seeds',
-    tableName: 'seeds',
-  },
-  asyncStackTraces: true,
-  acquireConnectionTimeout: 1000 * 8,
-  fetchAsString: [ 'clob', ],
-  useNullAsDefault: true,
-  log: {
-    warn(message) { debug(`[warn]: ${ message }`); },
-    error(message) { debug(`[error]: ${ message }`); },
-    deprecate(message) { debug(`[deprecate]: ${ message }`); },
-    debug(message) { debug(`[debug]: ${ message }`); },
-  },
-  // pool: {
-  //   min: 0,
-  //   max: 7,
-  // },
+export const log = {
+  warn(message) { debug(`[warn]: ${ message }`); },
+  error(message) { debug(`[error]: ${ message }`); },
+  deprecate(message) { debug(`[deprecate]: ${ message }`); },
+  debug(message) { debug(`[debug]: ${ message }`); },
 };
 
-module.exports = {
-  ...config,
-};
+// export const pool = {
+//   min: 0,
+//   max: 7,
+// };
--- be/db/knex/seeds/auth.js
+++ be/db/knex/seeds/auth.js
@@ -1,13 +1,11 @@
-module.exports = {
-  seed: async (knex) => {
-    await knex('auth').del();
-    await knex('auth').insert([
-      [ 1, 'admin', 'admin', ],
-      [ 2, 'user', 'user', ],
-    ].map(item => ({
-      authId: item[0],
-      login: item[1],
-      password: item[2],
-    })));
-  },
+export const seed = async (knex) => {
+  await knex('auth').del();
+  await knex('auth').insert([
+    [ 1, 'admin', 'admin', ],
+    [ 2, 'user', 'user', ],
+  ].map(item => ({
+    authId: item[0],
+    login: item[1],
+    password: item[2],
+  })));
 };
