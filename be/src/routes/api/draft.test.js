9d1819f21c117c900c319c93e6128574e3358afb
Nikita Shevchenko <nicksonsheva0609@gmail.com>
2020-09-03 00:13:22 +0300

    fast commit

--- be/src/routes/api/draft.test.js
+++ be/src/routes/api/draft.test.js
@@ -5,8 +5,6 @@ import frisby from 'frisby';
 import jest from 'jest';
 
 describe('draft', () => {
-    it('env is development', () => expect(env.NODE_ENV).toBe(env.NODE_ENV_EXPECTED));
-
     it('GET /api/draft success', async () => {
         await frisby
             .get(`${ env.URI_BASE }/api/draft`)
